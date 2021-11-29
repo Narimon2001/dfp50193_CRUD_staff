8f345704146da75bf606fd597a7004f0e3a88f27
Narimon2001 <naasengsuriya123@gmail.com>
2021-11-29 12:58:20 +0800

    latest crud

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
 </head>
 <body>
     <form action="login.php" method="post"> 
+    <center>
         <label for="idpengguna">ID Pengguna</label> 
         <input type="text" name="idpengguna" id="idpengguna"> 
         <label for="katalaluan">Kata Laluan</label> 
         <input type="password" name="katalaluan" id="katalaluan"> 
         <button type="submit">MASUK</button> 
+    </center>   
     </form> 
 </body>
 </html>
